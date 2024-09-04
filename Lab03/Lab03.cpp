3a748a135992b0aa806dddbcb9f8a4a3b5a96949
mill3074 <mill3074@purdue.edu>
2024-09-04 14:54:42 -0400

    i guess thats it

--- Lab03/Lab03.cpp
+++ Lab03/Lab03.cpp
@@ -1,20 +1,52 @@
-// Lab03.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to the Amazing Calculation Expert (A.C.E) 2.0!" << endl; 
+	cout << "" << endl;
+	cout << "Select an operation by inputting its corresponding digit." << endl;
+	cout << "1. Add (A + B)" << endl;
+	cout << "2. Subtract (A - B)" << endl;
+	cout << "3. Multiply (A * B)" << endl;
+	cout << "4. Divide (A / B)" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void getChoices(float& A, float& B) {
+	cout << "Please enter variable A: ";
+	cin >> A;
+	cout << "Please enter variable B: ";
+	cin >> B;
+	// The rest of this function is an exercise to the reader
+}
+void firstChoice(float A, float B) {
+	cout << A << " + " << B << " = " << A+B << endl;
+}
+void secondChoice(float A, float B) {
+	cout << A << " - " << B << " = " << A-B << endl;
+}
+void thirdChoice(float A, float B) {
+	cout << A << " * " << B << " = " << A*B << endl;
+}
+void fourthChoice(float A, float B) {
+	cout << A << " / " << B << " = " << A/B << endl;
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		firstChoice(A, B);
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+	if (choice == 4) {
+		fourthChoice(A, B);
+	}
+	return 0;
+}
